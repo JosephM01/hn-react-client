54be0469de1e9d500331eb7c32be7aa2a6ad4a86
Joseph <josephwow1@gamil.com>
2019-03-30 23:11:40 -0300

    Updates

---
+++ precache-manifest.cbe96eb1a56725e31b55f490af92d428.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab87b0e08a8ea4bcd856",
+    "url": "/hn-react-client/static/js/runtime~main.10864dd1.js"
+  },
+  {
+    "revision": "31cd9ef8d8ec7a84e6a0",
+    "url": "/hn-react-client/static/js/main.24b889f0.chunk.js"
+  },
+  {
+    "revision": "4a043237eca716faeb1c",
+    "url": "/hn-react-client/static/js/2.b0c59061.chunk.js"
+  },
+  {
+    "revision": "31cd9ef8d8ec7a84e6a0",
+    "url": "/hn-react-client/static/css/main.afa91680.chunk.css"
+  },
+  {
+    "revision": "c021a78ceb50c52dd3630c2c1065ea54",
+    "url": "/hn-react-client/index.html"
+  }
+];
